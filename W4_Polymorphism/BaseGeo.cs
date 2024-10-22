fab48cf7b8af63efbd0dca2713bb3d518c5a9304
AenuHub <jesuskane92@gmail.com>
2024-10-22 14:52:24 +0300

    Add project files.

---
+++ W4_Polymorphism/BaseGeo.cs
@@ -0,0 +1,34 @@
+﻿namespace W4_Polymorphism
+{
+    public class BaseGeoShape
+    {
+        private double _height;
+        private double _width;
+
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public virtual double Area()
+        {
+            return Height * Width;
+        }
+    }
+
+    public class Square : BaseGeoShape
+    {
+        // no need to override Area()
+    }
+
+    public class Rectangle : BaseGeoShape
+    {
+        // no need to override Area()
+    }
+
+    public class RightTriangle : BaseGeoShape
+    {
+        public override double Area()
+        {
+            return (Height * Width) / 2;
+        }
+    }
+}
---
+++ W4_Polymorphism/Program.cs
@@ -0,0 +1,24 @@
+﻿using W4_Polymorphism;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Square square = new Square();
+        square.Height = 20;
+        square.Width = 20;
+        Console.WriteLine($"The area of the square is {square.Area()}");
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.Height = 20;
+        rectangle.Width = 30;
+        Console.WriteLine($"The area of the rectangle is {rectangle.Area()}");
+
+        RightTriangle rightTriangle = new RightTriangle();
+        rightTriangle.Height = 20;
+        rightTriangle.Width = 30;
+        Console.WriteLine($"The area of the right triangle is {rightTriangle.Area()}");
+
+        Console.ReadKey();
+    }
+}
